08db41c37dcc1bad3f60c4a2b8d6f6ea2ece7ac7
merd13 <mehrdad.mans@outlook.com>
2018-07-25 21:18:15 -0500

    first commit

---
+++ src/components/Cell.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cell = () => {
+  return(
+    <div style={{display: 'flex'}}>
+    <div style={{borderStyle: 'solid', width: '20px'}}> Table X is running! </div>
+    </div>
+  )
+};
+
+export default Cell;
---
+++ src/components/Table.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Cell from './Cell.jsx';
+
+const Table = () => {
+  return(
+    <Cell/>
+  )
+};
+
+export default Table;
